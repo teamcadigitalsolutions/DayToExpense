ac4d5fd41f8b2c5f3858f4b933c3cf45a96dcf4f
teamca <teamcadigitalsolutions@gmail.com>
2026-09-01 20:50:11 +0530

    git ignore created

--- src/services/transactionService.ts
+++
@@ -1,9 +0,0 @@
-import api from '../lib/axios';
-
-export const transactionService = {
-  getTransactions: (workspaceId: string, params: any) => api.get(`/workspaces/${workspaceId}/transactions`, { params }),
-  createTransaction: (workspaceId: string, data: any) => api.post(`/workspaces/${workspaceId}/transactions`, data),
-  updateTransaction: (workspaceId: string, id: string, data: any) => api.put(`/workspaces/${workspaceId}/transactions/${id}`, data),
-  deleteTransaction: (workspaceId: string, id: string) => api.delete(`/workspaces/${workspaceId}/transactions/${id}`),
-  getTransaction: (workspaceId: string, id: string) => api.get(`/workspaces/${workspaceId}/transactions/${id}`),
-};
